4cf46e071266353dd591f5ff6a78270cf521f7d7
mtrickett <melanietrickett@gmail.com>
2024-04-06 18:55:53 -0400

    add counter and checkboxes

--- src/components/TodosList.tsx
+++ src/components/TodosList.tsx
@@ -15,17 +15,29 @@ export const TodosList = ({ completed }: TodosListProps) => {
   const todosList =
     todos.length > 0 ? todos.filter((todo) => todo.completed == completed) : [];
 
+  // if (todosList.length == 0) return null;
+
   return (
     <>
-      <h2 className="text-2xl font-semibold">{heading}</h2>
+      <h2 className="inline-block text-2xl font-semibold">{heading}</h2>
+      <span className="ml-3 inline-block w-8 rounded-full bg-gray-700 p-2 text-center align-bottom text-xs font-semibold text-white">
+        {todosList.length}
+      </span>
       {!isLoading && !isError && (
-        <ul>
+        <ul className="m-0 flex flex-col divide-y divide-slate-200 border-t border-slate-200">
           {todosList.length > 0 ? (
             todosList.map((todo) => {
-              return <li key={todo.id}>{todo.title}</li>;
+              return (
+                <li key={todo.id} className="py-2.5">
+                  <label className="text-md -mx-6 block rounded-xl border border-white p-4 font-medium hover:cursor-pointer hover:border-stone-200 hover:bg-stone-50">
+                    <input type="checkbox" checked={todo.completed} />
+                    {todo.title}
+                  </label>
+                </li>
+              );
             })
           ) : (
-            <p>{emptyMessage}</p>
+            <p className="my-4">{emptyMessage}</p>
           )}
         </ul>
       )}
