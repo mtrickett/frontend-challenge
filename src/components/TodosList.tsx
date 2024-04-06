6143d04645067fef0f8843fe6de664c7398b78e8
mtrickett <melanietrickett@gmail.com>
2024-04-06 15:45:50 -0400

    show todos in correct list

--- src/components/TodosList.tsx
+++ src/components/TodosList.tsx
@@ -9,12 +9,17 @@ export const TodosList = ({ completed }: TodosListProps) => {
 
   const heading = completed ? "Completed" : "Incomplete";
 
+  const todosList =
+    todos.length > 0
+      ? todos.filter((todo) => todo.completed == completed)
+      : null;
+
   return (
     <>
       <h3>{heading}</h3>
       <ul>
-        {todos.length > 0
-          ? todos.map((todo) => {
+        {todosList
+          ? todosList.map((todo) => {
               return (
                 <li key={todo.id}>
                   {todo.title} - {todo.completed.toString()}
